c745d95e4f77f8e755ea16a047cdc5c7ba8ea3f3
Advath Coder <72031535+Advaith6@users.noreply.github.com>
2021-09-14 15:04:08 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-const { fabric } = require("./fabric");
-
 var canvas = new fabric.Canvas('myCanvas');
 
 ball_y = 0;
@@ -12,7 +10,7 @@ block_image_width = 5;
 block_image_height = 5;
 
 function load_img(){
-	fabric.Image.fromURL("golf-h1.png", function(Img){
+	fabric.Image.fromURL("golf-h.png", function(Img){
 		hole_obj = Img;
 		hole_obj.scaleToWidth(50);
 		hole_obj.scaleToHeight(50);
